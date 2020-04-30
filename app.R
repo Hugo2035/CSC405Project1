0936e37430ccc21ba18f2cbf7aac7cbe14a8726c
Hugo2035 <52550107+Hugo2035@users.noreply.github.com>
2020-04-30 02:33:58 -0500

    Add files via upload

---
+++ app.R
@@ -0,0 +1,23 @@
+#CSC405 project1
+# This is a Shiny web application for interacting with H1-B Data.
+# Author: Hugues mbumba
+#########---######---
+
+
+#Loading dependent packages  ----
+require(shiny)
+rqrd_Pkg = c('shiny','plotly','plyr','tidyverse','ggmap')
+for(p in rqrd_Pkg){
+  if(!require(p,character.only = TRUE)) 
+  install.packages(p);
+  library(p,character.only = TRUE)
+}
+
+#source("global.R")
+source("ui.R")
+source("server.R")
+
+# Run the application 
+shinyApp(ui = ui, server = server)
+
+
